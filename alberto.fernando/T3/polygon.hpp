3759af92a55a7ec6595d46981304c48fa0ec410d
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-05-29 08:40:21 -0300

    data struture definitions

---
+++ alberto.fernando/T3/polygon.hpp
@@ -0,0 +1,15 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <istream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+namespace alberto {
+
+struct Point
+{
+  int x = 0;
+  int y = 0;
+};
