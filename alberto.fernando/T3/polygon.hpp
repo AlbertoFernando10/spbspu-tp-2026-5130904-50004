6a487fc8bc802781b6769a6601acd5cbd24213dc
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-05-29 08:51:07 -0300

    manipulation and query commands

--- alberto.fernando/T3/polygon.hpp
+++ alberto.fernando/T3/polygon.hpp
@@ -27,3 +27,20 @@ bool operator==(const Point& a, const Point& b);
 bool operator==(const Polygon& a, const Polygon& b);
 
 double polygonArea(const Polygon& poly);
+
+void cmdArea   (std::ostream& out, const std::vector< Polygon >& polys,
+                const std::string& arg);
+void cmdMax    (std::ostream& out, const std::vector< Polygon >& polys,
+                const std::string& arg);
+void cmdMin    (std::ostream& out, const std::vector< Polygon >& polys,
+                const std::string& arg);
+void cmdCount  (std::ostream& out, const std::vector< Polygon >& polys,
+                const std::string& arg);
+void cmdEcho   (std::ostream& out, std::vector< Polygon >& polys,
+                std::istream& in);
+void cmdRmecho (std::ostream& out, std::vector< Polygon >& polys,
+                std::istream& in);
+
+}
+
+#endif
